d0c107918b48ef3373cb4b577bebbdf9dd7e93e1
MelindaKleszken <melindakleszken1021@gmail.com>
2021-01-06 22:06:04 +0000

    need to work on image url change

--- script.js
+++ script.js
@@ -1,21 +1,58 @@
 const pokeImage = document.getElementById("poke-image");
 const pokeName = document.getElementById("name");
+const button = document.getElementById("reveal");
 
 fetch("https://pokeapi.co/api/v2/pokemon/")
     .then((response) => response.json())
     .then((inf) => {
-        //console.log(data);
+        //console.log(inf);
         DisplayData(inf);
 });
 
 //for update => when questionmark clicked => 1.reveal name 2. 1sec after show image of pokemon
 
+//create random number for user; number will be used to return a pokemon name and image
 const i = Math.floor(Math.random() * 20);
 console.log(i);
-//pokeName.innerHTML =  data.results[i].name;
 
 const DisplayData = (inf) => {
-    pokeName.textContent = inf.results[i].name;
+    pokeName.textContent = inf.results[i].name;     
 }
+
+//////make sure to retireve the updated name not the default ....!!!!!!!
+let newName = pokeName; 
+console.log(newName);
+let nameValue = newName.innerHTML;
+console.log(nameValue);
+
+fetch(`https://pokeapi.co/api/v2/pokemon/${newName}/`)
+    .then((response) => response.json())
+    .then((pic) => {
+        DisplayImage(pic);
+});
+
+//retrieve image from sprites
+const DisplayImage = (pic) => {
+    console.log(pic);
+    pokeImage.src = pic.sprites.front_shiny; 
+    
+}
+
+//2. match image with name
+//need to change questionamark url to pokemon specific url
+button.addEventListener("click", ()=>{
+    if (button.innerHTML == "Reveal") {
+        button.innerHTML = "Another";
+
+        //DisplayData(inf);
+    }
+
+});
+
+
+
+
+
+
 
 
