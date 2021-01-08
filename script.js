d9d2f4a87971510cef9bef35fb17ddb1d1fabccf
MelindaKleszken <melindakleszken1021@gmail.com>
2021-01-08 12:16:44 +0000

    final commit

--- script.js
+++ script.js
@@ -2,26 +2,7 @@ const pokeImage = document.getElementById("poke-image");
 const pokeName = document.getElementById("name");
 const button = document.getElementById("reveal");
 
-/*fetch("https://pokeapi.co/api/v2/pokemon/")
-    .then((response) => response.json())
-    .then((inf) => {
-        //console.log(inf);
-        DisplayData(inf);
-});*/
 
-//for update => when questionmark clicked => 1.reveal name 2. 1sec after show image of pokemon
-
-
-
-/*fetch(`https://pokeapi.co/api/v2/pokemon/${pokeName}/`)
-    .then((response) => response.json())
-    .then((pic) => {
-        DisplayImage(pic);
-});*/
-
-
-//2. match image with name
-//need to change questionamark url to pokemon specific url
 button.addEventListener("click", ()=>{
     if (button.innerHTML == "Reveal") {
         button.innerHTML = "Another";
@@ -35,19 +16,23 @@ button.addEventListener("click", ()=>{
     });
 
     //GET THE RANDOM NUMBER TO GENERATE DATA
-    const i = Math.floor(Math.random() * 20);
+    const i = Math.floor(Math.random() * 20) + 1; 
+    console.log(i);
     
     //TO UPDATE NAME AREA
     const DisplayData = (inf) => {
         pokeName.innerText = inf.results[i].name;     
         console.log(pokeName.textContent);
     }   
-    
+    //2. match image with name
+    //need to change questionamark url to pokemon specific url
     //get the Pokemon image
-    fetch(`https://pokeapi.co/api/v2/pokemon/${i}/`)
+    fetch(`https://pokeapi.co/api/v2/pokemon/${i+1}/`)
     .then((response) => response.json())
     .then((pic) => {
         DisplayImage(pic);
+        pokeImage.style.width = '17vh';
+        
     });
 
     //retrieve image from sprites
